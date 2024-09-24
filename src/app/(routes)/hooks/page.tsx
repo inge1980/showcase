0228586f2385a19ea4f6a9d4c1b60e25e2a4be7b
Inge Pedersen <inge.pedersen.1980@gmail.com>
2024-09-24 11:17:28 +0200

    mobile grid

--- src/app/(routes)/hooks/page.tsx
+++ src/app/(routes)/hooks/page.tsx
@@ -8,18 +8,16 @@ const Hooks = () => {
   const { theme, toggleTheme } = useTheme();
   const [backgroundColor, setBackgroundColor] = useState('cyan');
   return (
-    <div className="block align-top items-baseline justify-items-center min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]">
-      <main className={"w-full flex flex-col row-start-2 bg-" + backgroundColor + "-800 items-center sm:items-start"}>
-        <Navigation />
+    <main className={"w-full flex flex-col row-start-2 bg-" + backgroundColor + "-800 items-center sm:items-start"}>
+      <Navigation />
 
-        <div className="grid grid-cols-4 gap-4 flex items-start mx-3 py-3">
-          <Card title="Alert" desc="Click to se an alert" button="Alert click" clickHandler={() => alert('Click alert')} />
-          <Card title="Console.log" desc="Seemingly no action, but it is hidden" button="Nothing..?" clickHandler={() => console.log('Hidden from plain view, but developers know..')} />
-          <Card title="useState" desc={`Toggle page color only.. (${backgroundColor})`} button="Toggle!" clickHandler={() => { setBackgroundColor(backgroundColor === 'cyan' ? 'amber' : 'cyan'); }} />
-          <Card title="useContext" desc={`and useLayoutEffect Toggle theme color: ${theme === "light" ? "light" : "dark"}`} button="Toggle!" clickHandler={() => { toggleTheme(); }} />
-        </div>
-      </main>
-    </div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-4 flex items-start mx-3 py-3">
+        <Card title="Alert" desc="Click to se an alert" button="Alert click" clickHandler={() => alert('Click alert')} />
+        <Card title="Console.log" desc="Seemingly no action, but it is hidden" button="Nothing..?" clickHandler={() => console.log('Hidden from plain view, but developers know..')} />
+        <Card title="useState" desc={`Toggle page color only.. (${backgroundColor})`} button="Toggle!" clickHandler={() => { setBackgroundColor(backgroundColor === 'cyan' ? 'amber' : 'cyan'); }} />
+        <Card title="useContext" desc={`and useLayoutEffect Toggle theme color: ${theme === "light" ? "light" : "dark"}`} button="Toggle!" clickHandler={() => { toggleTheme(); }} />
+      </div>
+    </main>
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,20 @@ import React from 'react';
 
 const Home = () => {
   return (
-      <div className={"block align-top items-baseline justify-items-center min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]"}>
-        <main className="w-full flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <Navigation />
+      <main className="w-full flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Navigation />
 
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href="#"
-              rel="noopener noreferrer"
-              onClick={() => { alert('Hello world!'); }}
-              >
-              Hello React
-            </a>
-          </div>
-        </main>
-      </div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="#"
+            rel="noopener noreferrer"
+            onClick={() => { alert('Hello world!'); }}
+            >
+            Hello React
+          </a>
+        </div>
+      </main>
   );
 }
 
